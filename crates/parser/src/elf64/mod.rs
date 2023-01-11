bdb02afcf2e816d0b1e762819dee85db101d84ad
Ivan Enderlin <ivan@mnt.io>
2023-01-11 11:03:28 +0100

    feat(parser): Parse ELF64 `type` and `machine`.

--- crates/parser/src/elf64/mod.rs
+++ crates/parser/src/elf64/mod.rs
@@ -4,81 +4,225 @@ use weld_parser_macros::EnumParse;
 #[derive(EnumParse, Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Endianness {
-    Little = 0x1,
-    Big = 0x2,
+    // Little.
+    Little = 0x01,
+    // Big
+    Big = 0x02,
 }
 
 #[derive(EnumParse, Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum OsAbi {
-    // [System V](https://en.wikipedia.org/wiki/System_V).
+    /// [System V](https://en.wikipedia.org/wiki/System_V).
     SystemV = 0x00,
-
-    // [HP-UX](https://en.wikipedia.org/wiki/HP-UX).
+    /// [HP-UX](https://en.wikipedia.org/wiki/HP-UX).
     HpUx = 0x01,
-
-    // [NetBSD](https://en.wikipedia.org/wiki/NetBSD).
+    /// [NetBSD](https://en.wikipedia.org/wiki/NetBSD).
     NetBsd = 0x02,
-
-    // [GNU Linux](https://en.wikipedia.org/wiki/Linux).
+    /// [GNU Linux](https://en.wikipedia.org/wiki/Linux).
     Gnu = 0x03,
-
-    // [GNU Hurd](https://en.wikipedia.org/wiki/GNU_Hurd).
+    /// [GNU Hurd](https://en.wikipedia.org/wiki/GNU_Hurd).
     GnuHurd = 0x04,
-
-    // [Sun Solaris](https://en.wikipedia.org/wiki/Solaris_(operating_system)).
+    /// [Sun Solaris](https://en.wikipedia.org/wiki/Solaris_(operating_system)).
     Solaris = 0x06,
-
-    // [IBM AIX (Monterey)](https://en.wikipedia.org/wiki/IBM_AIX).
+    /// [IBM AIX (Monterey)](https://en.wikipedia.org/wiki/IBM_AIX).
     Aix = 0x07,
-
-    // [SGI IRIX](https://en.wikipedia.org/wiki/IRIX).
+    /// [SGI IRIX](https://en.wikipedia.org/wiki/IRIX).
     Irix = 0x08,
-
-    // [FreeBSD](https://en.wikipedia.org/wiki/FreeBSD).
+    /// [FreeBSD](https://en.wikipedia.org/wiki/FreeBSD).
     FreeBsd = 0x09,
-
-    // [Compaq TRU64 UNIX](https://en.wikipedia.org/wiki/Tru64).
+    /// [Compaq TRU64 UNIX](https://en.wikipedia.org/wiki/Tru64).
     Tru64 = 0x0a,
-
-    // Novell Modesto.
+    /// Novell Modesto.
     Modesto = 0x0b,
-
-    // [OpenBSD](https://en.wikipedia.org/wiki/OpenBSD).
+    /// [OpenBSD](https://en.wikipedia.org/wiki/OpenBSD).
     OpenBsd = 0x0c,
-
-    // [OpenVMS](https://en.wikipedia.org/wiki/OpenVMS).
+    /// [OpenVMS](https://en.wikipedia.org/wiki/OpenVMS).
     OpenVms = 0x0d,
-
-    // [Hewlett-Packard Non-Stop Kernel](https://en.wikipedia.org/wiki/NonStop_(server_computers)).
+    /// [Hewlett-Packard Non-Stop Kernel](https://en.wikipedia.org/wiki/NonStop_(server_computers)).
     Nsk = 0x0e,
-
-    // [AROS](https://en.wikipedia.org/wiki/AROS_Research_Operating_System).
+    /// [AROS](https://en.wikipedia.org/wiki/AROS_Research_Operating_System).
     Aros = 0x0f,
-
-    // FenixOS.
+    /// FenixOS.
     FenixOs = 0x10,
-
-    // [Nuxi CloudABI](https://en.wikipedia.org/wiki/CloudABI).
+    /// [Nuxi CloudABI](https://en.wikipedia.org/wiki/CloudABI).
     CloudAbi = 0x11,
-
-    // [Stratus Technologies OpenVOS](https://en.wikipedia.org/wiki/Stratus_VOS).
+    /// [Stratus Technologies OpenVOS](https://en.wikipedia.org/wiki/Stratus_VOS).
     OpenVos = 0x12,
-
-    // ARM EABI.
+    /// ARM EABI.
     ArmAeabi = 0x40,
-
-    // ARM.
+    /// ARM.
     Arm = 0x61,
-
-    // Standalone (embedded) application.
+    /// Standalone (embedded) application.
     Standalone = 0xff,
+}
+
+#[derive(EnumParse, Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u16)]
+pub enum Type {
+    /// Unknown.
+    None = 0x00,
+
+    /// Relocatable file.
+    RelocatableFile = 0x01,
+
+    /// Executable file.
+    ExecutableFile = 0x02,
+
+    /// Shared object.
+    SharedObject = 0x03,
+
+    /// Core file.
+    CoreFile = 0x04,
+}
+
+#[derive(EnumParse, Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u16)]
+pub enum Machine {
+    /// No specific instruction set.
+    None = 0x00,
+    /// [AT&T WE 32100](https://en.wikipedia.org/wiki/Bellmac_32).
+    Att32 = 0x01,
+    /// [SPARC](https://en.wikipedia.org/wiki/SPARC).
+    Sparc = 0x02,
+    /// [x86](https://en.wikipedia.org/wiki/X86).
+    X86 = 0x03,
+    /// [Motorola 68000 (M68k)](https://en.wikipedia.org/wiki/Motorola_68000_series).
+    Motorola68k = 0x04,
+    /// [Motorola 88000 (M88k)](https://en.wikipedia.org/wiki/Motorola_88000).
+    Motorola88k = 0x05,
+    /// [Intel MCU](https://en.wikipedia.org/wiki/List_of_Intel_processors#Microcontrollers).
+    IntelMcu = 0x06,
+    /// [Intel 80860](https://en.wikipedia.org/wiki/Intel_i860).
+    Intel860 = 0x07,
+    /// [MIPS](https://en.wikipedia.org/wiki/MIPS_architecture).
+    Mips = 0x08,
+    /// [IBM System/370](https://en.wikipedia.org/wiki/IBM_System/370).
+    IbmS370 = 0x09,
+    /// [MIPS RS3000 Little-endian](https://en.wikipedia.org/wiki/R3000).
+    MipsRs3Le = 0x0a,
+    /// [Hewlett-Packard PA-RISC](https://en.wikipedia.org/wiki/PA-RISC).
+    HpPaRisc = 0x0e,
+    /// Fujitsu VPP500..
+    FujitsuVpp500 = 0x11,
+    /// Sun's “v8plus”.
+    Sparc32Plus = 0x12,
+    /// [Intel 80960](https://en.wikipedia.org/wiki/Intel_i960).
+    Intel960 = 0x13,
+    /// [PowerPC](https://en.wikipedia.org/wiki/PowerPC).
+    PowerPc = 0x14,
+    /// [PowerPC](https://en.wikipedia.org/wiki/PowerPC) (64-bit).
+    PowerPc64 = 0x15,
+    /// [S390](https://en.wikipedia.org/wiki/Z/Architecture), including S390x.
+    IbmS390 = 0x16,
+    /// IBM SPU/SPC.
+    IbmSpu = 0x17,
+    /// [NEC V800](https://en.wikipedia.org/wiki/V850).
+    NecV800 = 0x24,
+    /// Fujitsu FR20.
+    FujitsuFr20 = 0x25,
+    /// [TRW RH-32](https://en.wikipedia.org/wiki/RH-32).
+    TrwRh32 = 0x26,
+    /// Motorola RCE.
+    MotorolaRce = 0x27,
+    /// [Arm](https://en.wikipedia.org/wiki/ARM_architecture_family) (up to Armv7/AArch32).
+    Arm = 0x28,
+    /// [Digital Alpha](https://en.wikipedia.org/wiki/Digital_Alpha).
+    DigitalAlpha = 0x29,
+    /// [SuperH](https://en.wikipedia.org/wiki/SuperH).
+    HitachiSuperH = 0x2a,
+    /// [SPARC Version 9](https://en.wikipedia.org/wiki/SPARC).
+    SparcV9 = 0x2b,
+    /// [Siemens TriCore embedded processor](https://en.wikipedia.org/wiki/Infineon_TriCore).
+    SiemensTricore = 0x2c,
+    /// [Argonaut RISC Core](https://en.wikipedia.org/wiki/ARC_(processor)).
+    ArgonautRiscCore = 0x2d,
+    /// [Hitachi H8/300](https://en.wikipedia.org/wiki/H8_Family).
+    HitachiH8300 = 0x2e,
+    /// [Hitachi H8/300H](https://en.wikipedia.org/wiki/H8_Family).
+    HitachiH8_300H = 0x2f,
+    /// [Hitachi H8S](https://en.wikipedia.org/wiki/H8_Family).
+    HitachiH8S = 0x30,
+    /// [Hitachi H8/500](https://en.wikipedia.org/wiki/H8_Family).
+    HitachiH8_500 = 0x31,
+    /// [IA-64](https://en.wikipedia.org/wiki/IA-64).
+    IntelA64 = 0x32,
+    /// [Stanford MIPS-X](https://en.wikipedia.org/wiki/MIPS-X).
+    MipsX = 0x33,
+    /// [Motorola ColdFire](https://en.wikipedia.org/wiki/NXP_ColdFire).
+    MotorolaColdfire = 0x34,
+    /// [Motorola M68HC12](https://en.wikipedia.org/wiki/Motorola_68HC12).
+    MotorolaM68Hc12 = 0x35,
+    /// Fujitsu MMA Multimedia Accelerator.
+    FujitsuMma = 0x36,
+    /// Siemens PCP.
+    SiemensPcp = 0x37,
+    /// [Sony nCPU embedded RISC processor](https://en.wikipedia.org/wiki/Cell_(microprocessor)).
+    SonuNcpu = 0x38,
+    /// Denso NDR1 microprocessor.
+    DensoNdr1 = 0x39,
+    /// Motorola Star\*Core processor.
+    MotorolaStarCore = 0x3a,
+    /// Toyota ME16 processor.
+    ToyotaMe16 = 0x3b,
+    /// STMicroelectronics ST100 processor.
+    St100 = 0x3c,
+    /// Advanced Logic Corp. TinyJ embedded processor family.
+    TinyJ = 0x3d,
+    /// [AMD x86-64](https://en.wikipedia.org/wiki/Amd64).
+    X86_64 = 0x3e,
+    /// Sony DSP Processor.
+    SonyDSP = 0x3f,
+    /// [Digital Equipment Corp. PDP-10](https://en.wikipedia.org/wiki/PDP-10).
+    Pdp10 = 0x40,
+    /// [Digital Equipment Corp. PDP-11](https://en.wikipedia.org/wiki/PDP-11).
+    Pdp11 = 0x41,
+    /// Siemens FX66 microcontroller.
+    SiemensFx66 = 0x42,
+    /// STMicroelectronics ST9+ 8/16 bit microcontroller.
+    St9Plus = 0x43,
+    /// STMicroelectronics ST7 8-bit microcontroller.
+    St7 = 0x44,
+    /// [Motorola MC68HC16 Microcontroller](https://en.wikipedia.org/wiki/Motorola_68HC16).
+    Motorola68Hc16 = 0x45,
+    /// [Motorola MC68HC11 Microcontroller](https://en.wikipedia.org/wiki/Motorola_68HC11).
+    Motorola68Hc11 = 0x46,
+    /// [Motorola MC68HC08 Microcontroller](https://en.wikipedia.org/wiki/Motorola_68HC08).
+    Motorola68Hc08 = 0x47,
+    /// [Motorola MC68HC05 Microcontroller](https://en.wikipedia.org/wiki/Motorola_68HC05).
+    Motorola68Hc05 = 0x48,
+    /// Silicon Graphics SVx.
+    Svx = 0x49,
+    /// STMicroelectronics ST19 8-bit microcontroller.
+    St19 = 0x4a,
+    /// [Digital VAX](https://en.wikipedia.org/wiki/VAX).
+    Vax = 0x4b,
+    /// Axis Communications 32-bit embedded processor.
+    Cris = 0x4c,
+    /// Infineon Technologies 32-bit embedded processor.
+    Javelin = 0x4d,
+    /// Element 14 64-bit DSP Processor.
+    FirePath = 0x4e,
+    /// LSI Logic 16-bit DSP Processor.
+    Zsp = 0x4f,
+    /// [TMS320C6000 Family](https://en.wikipedia.org/wiki/Texas_Instruments_TMS320#C6000_series).
+    TiC6000 = 0x8c,
+    /// [MCST Elbrus e2k](https://en.wikipedia.org/wiki/Elbrus_2000).
+    McstElbrus = 0xaf,
+    /// [Arm 64-bits](https://en.wikipedia.org/wiki/AArch64) (Armv8/AArch64).
+    Aarch64 = 0xb7,
+    /// [RISC-V](https://en.wikipedia.org/wiki/RISC-V).
+    RiscV = 0xf3,
+    /// [Berkeley Packet Filter](https://en.wikipedia.org/wiki/Berkeley_Packet_Filter).
+    Bpf = 0xf7,
 }
 
 #[derive(Debug)]
 pub struct File {
     pub endianness: Endianness,
     pub os_abi: OsAbi,
+    pub r#type: Type,
+    pub machine: Machine,
 }
 
 impl File {
@@ -88,16 +232,24 @@ impl File {
     where
         E: ParseError<Input<'a>>,
     {
-        let (input, (_magic, _class, endianness, _version, os_abi, _padding)) = tuple((
-            tag(Self::MAGIC),
-            tag(&[0x2] /* 64 bits */),
-            Endianness::parse,
-            tag(&[0x1]),
-            OsAbi::parse,
-            skip(8usize),
-        ))(input)?;
+        let (input, (_magic, _class, endianness, _version, os_abi, _padding, r#type, machine)) =
+            tuple((
+                tag(Self::MAGIC),
+                tag(&[0x2] /* 64 bits */),
+                Endianness::parse,
+                tag(&[0x1]),
+                OsAbi::parse,
+                skip(8usize),
+                Type::parse,
+                Machine::parse,
+            ))(input)?;
 
-        let file = Self { endianness, os_abi };
+        let file = Self {
+            endianness,
+            os_abi,
+            r#type,
+            machine,
+        };
 
         Ok((input, file))
     }
