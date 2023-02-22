c68e5a9bad169c0ccd3ac2a1056dc44c52cad6b9
Ivan Enderlin <ivan@mnt.io>
2023-02-22 21:05:32 +0100

    feat(parser): Re-export some nom errors.

--- crates/object/src/lib.rs
+++ crates/object/src/lib.rs
@@ -7,3 +7,9 @@ pub use endianness::*;
 
 pub type Input<'a> = &'a [u8];
 pub type Result<'a, O, E> = nom::IResult<Input<'a>, O, E>;
+
+pub mod errors {
+    pub use nom::Err as Error;
+    pub type SingleError<'a> = nom::error::Error<super::Input<'a>>;
+    pub use nom::error::ErrorKind;
+}
