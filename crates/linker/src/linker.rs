782d6e9518b512a3e3f72253a0ae0634ce39e48d
Ivan Enderlin <ivan@mnt.io>
2023-02-22 21:06:22 +0100

    feat(linker): Set basic work distribution and start parsing object files.

--- crates/linker/src/linker.rs
+++ crates/linker/src/linker.rs
@@ -1,10 +1,8 @@
-use std::num::NonZeroUsize;
+use std::{io, num::NonZeroUsize};
 
-use smol::{
-    block_on,
-    channel::{unbounded, Sender},
-    Executor,
-};
+use smol::{block_on, channel::unbounded};
+use thiserror::Error;
+use weld_file::{FileReader, Picker as FilePicker};
 use weld_scheduler::ThreadPool;
 
 use crate::Configuration;
@@ -14,16 +12,63 @@ pub struct Linker {
     configuration: Configuration,
 }
 
-#[derive(Debug, thiserror::Error)]
-pub enum Error {}
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("thread pool has failed: {0}")]
+    ThreadPool(io::Error),
+
+    #[error("thread pool's sender channel has been closed prematuraly")]
+    ThreadPoolChannelClosed,
+
+    #[error("parsing an object has failed: {0}")]
+    ObjectParser(weld_object::errors::Error<()>),
+}
 
 impl Linker {
     pub(crate) fn with_configuration(configuration: Configuration) -> Self {
         Self { configuration }
     }
 
-    #[must_use]
     pub fn link(self) -> Result<(), Error> {
-        Ok(())
+        // SAFETY: It's OK to `unwrap` as 4 is not 0.
+        let thread_pool =
+            ThreadPool::new(NonZeroUsize::new(4).unwrap()).map_err(Error::ThreadPool)?;
+
+        let (sender, receiver) = unbounded::<Result<(), Error>>();
+
+        for input_file_name in self.configuration.input_files {
+            let sender = sender.clone();
+
+            thread_pool
+                .execute(async move {
+                    let work = async move {
+                        dbg!(&input_file_name);
+                        let input_file = FilePicker::open(input_file_name).unwrap();
+
+                        let file_content = input_file.read_as_bytes().await.unwrap();
+                        let bytes: &[u8] = file_content.as_ref();
+                        dbg!(weld_object::elf64::File::parse(bytes).map_err(Error::ObjectParser)?);
+                        dbg!(std::thread::current().name());
+
+                        Ok(())
+                    };
+
+                    sender
+                        .send(work.await)
+                        .await
+                        .expect("work' sender channel has been closed prematuraly");
+                })
+                .map_err(|_| Error::ThreadPoolChannelClosed)?;
+        }
+
+        drop(sender);
+
+        block_on(async {
+            while let Ok(received) = receiver.recv().await {
+                dbg!(&received);
+            }
+
+            Ok(())
+        })
     }
 }
