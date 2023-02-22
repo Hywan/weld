5dec904e3fcc9738c325acc8021248a3df9e51d3
Ivan Enderlin <ivan@mnt.io>
2023-02-22 21:04:29 +0100

    feat(scheduler): Allow `Worker` to return any kind of results.

--- crates/scheduler/src/lib.rs
+++ crates/scheduler/src/lib.rs
@@ -1,5 +1,6 @@
 use std::{
     future::Future,
+    io,
     num::NonZeroUsize,
     pin::Pin,
     thread::{self, JoinHandle},
@@ -11,29 +12,32 @@ use smol::{
     Executor,
 };
 
-pub struct ThreadPool<'e> {
+pub struct ThreadPool<'e, T> {
     _workers: Vec<Worker>,
     executor: Executor<'e>,
-    sender: Sender<Job>,
+    sender: Sender<Job<T>>,
 }
 
-impl<'e> ThreadPool<'e> {
-    pub fn new(pool_size: NonZeroUsize) -> Self {
+impl<'e, T> ThreadPool<'e, T>
+where
+    T: Send + 'static,
+{
+    pub fn new(pool_size: NonZeroUsize) -> Result<Self, io::Error> {
         let pool_size: usize = pool_size.into();
         let mut workers = Vec::with_capacity(pool_size);
 
-        let (sender, receiver) = unbounded::<Job>();
+        let (sender, receiver) = unbounded::<Job<T>>();
 
         for nth in 0..pool_size {
-            workers.push(Worker::new(nth, receiver.clone()));
+            workers.push(Worker::new(nth, receiver.clone())?);
         }
 
-        Self { _workers: workers, executor: Executor::new(), sender }
+        Ok(Self { _workers: workers, executor: Executor::new(), sender })
     }
 
-    pub fn execute<F>(&self, work: F) -> Result<(), SendError<Job>>
+    pub fn execute<F>(&self, work: F) -> Result<(), SendError<Job<T>>>
     where
-        F: Future<Output = ()> + Send + 'static,
+        F: Future<Output = T> + Send + 'static,
     {
         block_on(self.executor.run(self.sender.send(Box::pin(work))))
     }
@@ -44,10 +48,13 @@ struct Worker {
     _thread_handle: JoinHandle<()>,
 }
 
-type Job = Pin<Box<dyn Future<Output = ()> + Send + 'static>>;
+type Job<T> = Pin<Box<dyn Future<Output = T> + Send + 'static>>;
 
 impl Worker {
-    fn new(worker_id: usize, receiver: Receiver<Job>) -> Self {
+    fn new<E>(worker_id: usize, receiver: Receiver<Job<E>>) -> Result<Self, io::Error>
+    where
+        E: Send + 'static,
+    {
         let thread_handle = thread::Builder::new()
             .name(format!("{prefix}-worker-{worker_id}", prefix = env!("CARGO_PKG_NAME")))
             .spawn(move || {
@@ -58,9 +65,8 @@ impl Worker {
                         executor.spawn(received_future).detach();
                     }
                 }))
-            })
-            .unwrap();
+            })?;
 
-        Self { _id: worker_id, _thread_handle: thread_handle }
+        Ok(Self { _id: worker_id, _thread_handle: thread_handle })
     }
 }
