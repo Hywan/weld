0f3da46d60ef70d490610561b38c62e425bac5c9
Ivan Enderlin <ivan@mnt.io>
2024-10-29 09:09:06 +0100

    doc(linker): Improve the documentation of `Configuration`.

--- crates/linker/src/configuration.rs
+++ crates/linker/src/configuration.rs
@@ -7,8 +7,13 @@ use crate::{target::Triple, Linker};
 /// This type works like a builder for [`Linker`].
 #[derive(Debug)]
 pub struct Configuration {
+    /// The target triple for which the linker has to link.
     pub(crate) target: Triple,
+
+    /// All the files the linker has to link together.
     pub(crate) input_files: Vec<PathBuf>,
+
+    /// The file that will contain the result of the linker.
     pub(crate) _output_file: PathBuf,
 }
 
